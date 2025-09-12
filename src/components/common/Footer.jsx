c48deaccf80ae41739c8972054862aa700c178f4
Neha <nehaboora49@gmail.com>
2025-09-12 14:15:22 +0530

    created the footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import Container from './Container'
+import { Link } from 'react-router-dom'
+import Description from './Description'
+import { Footer_Links } from '../../utils/helper'
 
 const Footer = () => {
+  const Year = new Date();
   return (
-    <div>Footer</div>
+    <>
+      <Container className={'mb-[50px]'}>
+        <div className='flex justify-between sm:my-[50px] my-[30px] flex-wrap gap-y-6'>
+          <div className='lg:w-[25%] '>
+            <Link to={'/'} className='font-medium text-[30px] mb-[14px]'>
+              Logo
+            </Link>
+            <Description className={'sm:text-[20px]'}>
+              Rapkhen is an outstanding Nursery & Primary school targeting educational System, helping Kids to read and write and to establish strong identity.
+            </Description>
+          </div>
+          <div className='lg:w-[70%] flex md:gap-[48px] gap-[20px] max-[680px]:flex-col'>
+            {Footer_Links.map((item, index) => {
+              return (
+                <ul key={index} >
+                  <li className='font-600 sm:text-[25px] text-[20px] text-[#FF9534] mb-[20px] whitespace-nowrap leading-[21px]'>
+                    {item.title}
+                  </li>
+                  {item.links.map((linkItem, i) => {
+                    const Icon = linkItem.icon;
+                    const isExternal =
+                      linkItem.href.startsWith("http") || linkItem.href.startsWith("mailto") ||
+                      linkItem.href.startsWith("tel");
+
+                    // Choose alignment depending on the section title
+                    const alignment =
+                      item.title === "Social Contact" ? "items-center" : "items-start";
+
+                    return (
+                      <li
+                        key={i}
+                        className={`flex ${alignment} sm:gap-3 gap-2 mb-[16px]`}
+                      >
+                        {Icon && (
+                          <span className="w-6 h-6 flex-none mt-1">
+                            <Icon className="w-full h-full text-black" />
+                          </span>
+                        )}
+
+                        {isExternal ? (
+                          <a
+                            href={linkItem.href}
+                            className="sm:text-[20px]  font-normal hover:text-[#FF9534] transition-colors duration-100 ease-linear text-black"
+                          >
+                            {linkItem.text}
+                          </a>
+                        ) : (
+                          <Link
+                            to={linkItem.href}
+                            className="sm:text-[20px] font-normal hover:text-[#FF9534] transition-colors duration-100 ease-linear text-black"
+                          >
+                            {linkItem.text}
+                          </Link>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )
+            })}
+          </div>
+        </div>
+        <div>
+          <h4 className='font-medium sm:text-[17px] text-[16px] text-center text-[#808080] '>
+            Copyright © {Year.getFullYear()}. All Right Reserved.
+          </h4>
+        </div>
+      </Container >
+    </>
   )
 }
 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,6 @@
+import { href } from "react-router-dom"
+import { Compass, Email, Facebook, Instagram, Phone, Telegram, Twitter, Whatup } from "./icons"
+
 export const Navlinks = [
     { label: 'home', path: '/' },
     { label: 'about us', path: 'about' },
@@ -9,3 +12,86 @@ export const Navlinks = [
 
 export const capitalizeWords = (str) =>
     str.replace(/\b\w/g, (char) => char.toUpperCase())
+
+export const Footer_Links = [
+    {
+        title: 'Links',
+        links: [
+            {
+
+                text: 'Home',
+                href: '/',
+            },
+            {
+                text: 'About us',
+                href: 'about',
+            },
+            {
+                text: 'Management',
+                href: 'management',
+            },
+            {
+                text: 'News & Event',
+                href: 'news',
+            },
+            {
+                text: 'Gallery',
+                href: 'gallery',
+            },
+            {
+                text: 'Contact us',
+                href: 'contact',
+            },
+        ],
+    },
+    {
+        title: 'Find Us',
+        links: [
+            {
+                icon: Compass,
+                text: 'Lorem ipsum dolor siter amet, consectetur adipising elitsed do sum.',
+                href: '',
+            },
+            {
+                icon: Email,
+                text: 'Loremipilum11@gmail.com',
+                href: 'mailto:Loremipilum11@gmail.com',
+            },
+            {
+                icon: Phone,
+                text: '+234 805 029 1265+234 907 228 0656',
+                href: 'tel:+234 805 029 1265+234 907 228 0656',
+            },
+        ],
+    },
+    {
+        title: 'Social Contact',
+        links: [
+            {
+                icon: Facebook,
+                text: 'Facebook',
+                href: 'https://www.facebook.com/',
+            },
+            {
+                icon: Twitter,
+                text: 'Twitter',
+                href: 'https://twitter.com/?',
+            },
+            {
+                icon: Instagram,
+                text: 'Instagram',
+                href: 'https://www.instagram.com/',
+            },
+            {
+                icon: Whatup,
+                text: 'Whatsapp',
+                href: 'https://www.whatsapp.com/',
+            },
+            {
+                icon: Telegram,
+                text: 'Telegram',
+                href: 'https://web.telegram.org/',
+            },
+        ],
+    },
+]
